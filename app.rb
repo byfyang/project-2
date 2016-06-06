15804b5e52bd8ca8282e70d231ed2c4f0367f03e
byfyang <darkspherico@gmail.com>
2016-06-06 14:03:21 -0700

    create, read, delete working

--- app.rb
+++ app.rb
@@ -10,10 +10,34 @@ class TaiwanFood < Sinatra::Base
 		erb :"index"
 	end
 
+	get '/food/new' do
+		erb :new
+	end
+
+
+	post '/food' do
+		@foods = Food.create(params[:food])
+		@foods.save
+		redirect "/"
+	end
+
+
 	get '/food/:id' do
 		@foods = Food.all
 		@foods = Food.find(params[:id])
 		@foods.name
 	end
+
+	get '/food/show/:id' do
+		@foods = Food.find(params[:id])
+		erb :show
+	end
+
+	post '/food/:id/delete' do
+		@foods = Food.find(params[:id])
+		@foods.delete
+		redirect("/food")
+	end
+
 
 end
