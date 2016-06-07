af105e66337ec758b81e324a63a1116758b50a1b
byfyang <darkspherico@gmail.com>
2016-06-06 17:23:43 -0700

    individual id for each food shows

--- app.rb
+++ app.rb
@@ -1,51 +1,53 @@
 class TaiwanFood < Sinatra::Base
 
+#route to home page
 	get "/" do 
 		erb :"about"
 	end
 
-
+#route to all of the food in the databasse
 	get '/food' do
 		@foods = Food.all
 		erb :"index"
 	end
 
+#route to inputing new food into the database
 	get '/food/new' do
 		erb :new
 	end
 
-
+#post new items into database and then redirecting to food index page
 	post '/food' do
 		@foods = Food.create(params[:food])
-		@foods.save
 		redirect "/food"
 	end
 
-
+#route to show individual food based on food's ID
 	get '/food/:id' do
-		@foods = Food.all
-		@foods = Food.find(params[:id])
-		@foods.name
+		@food = Food.find(params[:id])
+		erb :"show"
 	end
 
-	get '/food/show/:id' do
-		@foods = Food.find(params[:id])
-		erb :show
-	end
+	# get '/food/show/:id' do
+	# 	@foods = Food.find(params[:id])
+	# 	erb :show
+	# end
 
+
+#delete route and then redirecting to food index page
 	post '/food/:id/delete' do
 		@foods = Food.find(params[:id])
 		@foods.delete
 		redirect("/food")
 	end
 
-  	# edit
+#edit route page
  	get '/food/:id/edit' do
    	 	@food = Food.find(params[:id])
     	erb(:"edit")
   	end
 
-  # update
+#update page when successful will redirect to food index page
 	post '/food/:id' do
     	@food = Food.find(params[:id])
     		if @food.update_attributes(params[:food])
