9a4bca51dc5f3a46bf2c244646a150c2b25d9ec8
byfyang <darkspherico@gmail.com>
2016-06-09 12:04:05 -0700

    99% done with CSS + Routes working

--- app.rb
+++ app.rb
@@ -1,11 +1,20 @@
 class TaiwanFood < Sinatra::Base
 
-#route to home page
-	get "/" do 
+# TaiwanFood.use Rack::MethodOverride
+
+#route to about page
+	get "/about" do 
 		erb :"about"
 	end
 
-###### ROUTE FOR DRINKS #######
+#route to home page
+	get '/' do
+		@Foods = Food.all
+		@randomFood = @Foods.sample
+		erb :'home'
+	end
+
+############### ROUTE FOR DRINKS ###################
 
 #route to all the drinks in the database
 	get '/drink' do
@@ -28,25 +37,49 @@ class TaiwanFood < Sinatra::Base
 	get '/drink/:id' do
 		@drinks = Drink.all
   		@drink = Drink.find(params[:id])
+  		erb :'/drinks/show'
 	end
 
+#delete route and then redirecting to drink index page
+	post '/drink/:id/delete' do
+		@drink = Drink.find(params[:id])
+		@drink.delete
+		redirect("/drink")
+	end
+
+#go to edit page
+ 	get '/drink/:id/edit' do
+   	 	@drink = Drink.find(params[:id])
+    	erb(:"/drinks/edit")
+  	end
+
+#update page when successful will redirect to drink index page
+	post '/drink/:id' do
+    	@drink = Drink.find(params[:id])
+    		if @drink.update_attributes(params[:drink])
+      	redirect("/drink")
+    		else
+      	erb(:"/drinks/edit")
+    	end
+  	end
 
 
-##########END OF ROUTES FOR DRINKS############
+############## END OF ROUTES FOR DRINKS #################
+
 
 ############## ROUTES FOR FOODS ############
 #route to all of the food in the databasse
 	get '/food' do
 		@foods = Food.all
-		erb :"index"
+		erb :"/foods/index"
 	end
 
 #route to inputing new food into the database
 	get '/food/new' do
-		erb :new
+		erb :"/foods/new"
 	end
 
-#post new items into database and then redirecting to food index page
+#create new items into database and then redirecting to food index page
 	post '/food' do
 		@foods = Food.create(params[:food])
 		redirect "/food"
@@ -55,7 +88,7 @@ class TaiwanFood < Sinatra::Base
 #route to show individual food based on food's ID
 	get '/food/:id' do
 		@food = Food.find(params[:id])
-		erb :"show"
+		erb :"/foods/show"
 	end
 
 #delete route and then redirecting to food index page
@@ -65,10 +98,10 @@ class TaiwanFood < Sinatra::Base
 		redirect("/food")
 	end
 
-#edit route page
+#go to edit page
  	get '/food/:id/edit' do
    	 	@food = Food.find(params[:id])
-    	erb(:"edit")
+    	erb(:"/foods/edit")
   	end
 
 #update page when successful will redirect to food index page
@@ -77,7 +110,7 @@ class TaiwanFood < Sinatra::Base
     		if @food.update_attributes(params[:food])
       	redirect("/food")
     		else
-      	erb(:"edit")
+      	erb(:"/foods/edit")
     	end
   	end
 ######## END OF ROUTE FOR FOOD ##########
